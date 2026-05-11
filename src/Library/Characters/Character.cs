2fb5be808db655cd8f54879ced24ce8fbf601db4
maximopiccirillo <maximo.piccirillo@fing.edu.uy>
2026-05-11 19:04:54 -0300

    Clase padre Character

---
+++ src/Library/Characters/Character.cs
@@ -0,0 +1,101 @@
+using System.Collections.Generic;
+namespace Ucu.Poo.RoleplayGame;
+
+public abstract class Character :ICharacter
+{
+    private List<IItem> items = new List<IItem>();
+
+    private int health = 100;
+    private Character(string name)
+    {
+        this.Name = name;
+    }
+    public string Name { get; set; }
+
+    public int AttackValue
+    {
+        get
+        {
+            
+            return this.SumAttackValue(this.items);
+
+        }
+    }
+    public int DefenseValue
+    {
+        get
+        {
+            
+            return this.SumDefenseValue(this.items);
+
+        }
+    }
+
+    public int Health
+    {
+        get
+        {
+            return this.health;
+        }
+        private set
+        {
+            this.health = value < 0 ? 0 : value;
+        }
+    }
+    public void ReceiveAttack(int attackValue)
+        {
+            if (this.DefenseValue < power)
+            {
+                this.Health -= power - this.DefenseValue;
+            }
+
+        }
+    public void Cure()
+    {
+        this.Health = 100;
+    }
+
+    public void AddItem(IItem item)
+    {
+        this.items.Add(item);
+    }
+
+    public void RemoveItem(IItem item)
+    {
+        this.items.Remove(item);
+    }
+
+    private int SumAttackValue(List<IItem> items)
+    {
+        int value = 0;
+
+        foreach (IItem item in items)
+        {
+            if (item is IAttackItem attackItem)
+            {
+                value += attackItem.AttackValue;
+            }
+        }
+
+        return value;
+    }
+
+    private int SumDefenseValue(List<IItem> items)
+    {
+        int value = 0;
+
+        foreach (IItem item in items)
+        {
+            if (item is IDefenseItem defenseItem)
+            {
+                value += defenseItem.DefenseValue;
+            }
+        }
+
+        return value;
+    }
+
+
+
+
+}
